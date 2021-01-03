ebe4e4f708eeb6a8c054ca3d41191d6f91d5e06e
unknown <andreas.alfven95@gmail.com>
2021-01-03 20:46:09 +0100

    Added and fixed fullpage.js, cleaned up project and code.

--- js/index.js
+++ js/index.js
@@ -27,33 +27,6 @@ function linkAction() {
 
 navLink.forEach((n) => n.addEventListener('click', linkAction))
 
-/* SCROLL SECTIONS ACTIVE LINK */
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-  const scrollY = window.pageYOffset
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight
-    //When to swap over to next active link
-    const sectionTop = current.offsetTop - 300
-
-    sectionId = current.getAttribute('id')
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link')
-    } else {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link')
-    }
-  })
-}
-
-window.addEventListener('scroll', scrollActive)
-
 /* CHANGE HEADER BACKGROUND */
 function scrollHeader() {
   const header = document.getElementById('header')
