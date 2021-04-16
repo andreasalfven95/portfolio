15899a39f6f73ebd6c890fabea6193e07a29a3fd
Andreas Alfvén <andreas.larsson95@gmail.com>
2021-04-16 14:39:30 +0200

    Changed gifs to mp4s/still pictures.

--- js/index.js
+++ js/index.js
@@ -23,6 +23,37 @@ function linkAction() {
 
 navLink.forEach((n) => n.addEventListener('click', linkAction))
 
+var videoAttr = { autoplay: true, loop: true, mute: true, playsinline: true }
+var imgMP4s = Array.prototype.map.call(
+  document.querySelectorAll('img[src*=".mp4"]'),
+  function (img) {
+    var src = img.src
+    img.src = null
+
+    img.addEventListener('error', function (e) {
+      console.log('MP4 in image not supported. Replacing with video', e)
+      var video = document.createElement('video')
+
+      for (var key in videoAttr) {
+        video.setAttribute(key, videoAttr[key])
+      }
+
+      for (
+        var imgAttr = img.attributes, len = imgAttr.length, i = 0;
+        i < len;
+        i++
+      ) {
+        video.setAttribute(imgAttr[i].name, imgAttr[i].value)
+      }
+
+      img.parentNode.insertBefore(video, img)
+      img.parentNode.removeChild(img)
+    })
+
+    img.src = src
+  }
+)
+
 /* SCROLL REVEAL ANIMATION */
 /* const sr = ScrollReveal({
   origin: 'top',
